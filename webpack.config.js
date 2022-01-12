1818c795e26c47bf3c50ec4fe4a5d914fc1f3451
Iurii Karpenkov <truloveu@gmail.com>
2022-01-12 19:14:51 +0300

    add assets support

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
 
-const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash]${ext}`
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -18,7 +19,7 @@ module.exports = {
     },
     devServer: {
         historyApiFallback: true,
-        // contentBase: path.resolve(__dirname, 'app'),
+        contentBase: path.resolve(__dirname, 'app'),
         open: true,
         compress: true,
         hot: true,
@@ -35,7 +36,12 @@ module.exports = {
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: `./css/${filename('css')}`
-        })
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, 'src/assets'), to: path.resolve(__dirname, 'app') }
+            ]
+        }),
     ],
     module: {
         rules: [
